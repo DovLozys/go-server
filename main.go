cfe407ff997a5664fd415b28c31359636cf0504a
Dovydas Lozys <reduktorius@gmail.com>
2022-04-05 15:00:07 +0100

    adds `helloHandler` function to handle `/hello`

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
-import(
+import (
 	"fmt"
 	"log"
 	"net/http"
 )
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 - Not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method not supported.", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Oh hai!")
+}
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
