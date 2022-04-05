c49cfa9db3894a25472669feb3f233a07e9b362d
Dovydas Lozys <reduktorius@gmail.com>
2022-04-05 14:50:23 +0100

    adds listen/serve functionality, defines routes

--- main.go
+++ main.go
@@ -7,5 +7,13 @@ import(
 )
 
 func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
 
+	fmt.Printf("Started the server at :8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
