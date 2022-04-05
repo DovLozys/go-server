58e70cba54d6f163e4963cc5958858d023bec794
Dovydas Lozys <reduktorius@gmail.com>
2022-04-05 16:53:15 +0100

    adds new line in form display

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "POST request successful.")
+	fmt.Fprintf(w, "POST request successful.\n")
 	name := r.FormValue("name")
 	address := r.FormValue("address")
 	fmt.Fprintf(w, "Name: %s\n", name)
